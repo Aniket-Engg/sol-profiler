625226469ed7162ef97d691789b0ab7634198d74
app <aniketengg.225@gmail.com>
2019-03-26 18:43:25 +0530

    linting

--- bin/sol-profiler.js
+++ bin/sol-profiler.js
@@ -5,16 +5,14 @@ const clc    = require('cli-color'),
      { generateAndStore }  = require('./../lib/generate'),
      {directory}  = require('./../utils');
 
-
-
 if(process.argv.length < 3) {
   console.log(clc.redBright("Error: No file or directory provided"));
   process.exit(1);
 }
 
 let path = process.argv[2];
-
-var profiler = async (path) => {
+/* jshint ignore:start */
+var profiler = async(path) => {
     try{
         if(fs.lstatSync(path).isDirectory()){
             let count = 0;
@@ -26,23 +24,25 @@ var profiler = async (path) => {
                 }
             }
             if(count)
-                console.log(clc.green('Info: Found '+ count + ' Solidity files!!! Generated profiles stored in '+ process.cwd() + '/profiles'))
+                console.log(clc.green('Info: Found '+ count + ' Solidity files!!! Generated profiles stored in '+ process.cwd() + '/profiles'));
             else
-                console.log(clc.green('Info: No Solidity files found in directory'))
+                console.log(clc.green('Info: No Solidity files found in directory'));
         }
         else{
             if(path.substr(-4) == '.sol'){
                 let tableData = await generateAndStore(path);
+                
                 //Render profile on console
                 console.log(tableData);
             }
             else
-                console.warn(clc.yellow('Warning: Please provide a solidity file'))
+                console.warn(clc.yellow('Warning: Please provide a solidity file'));
         }
     }catch(error){
         console.error(clc.redBright('Error in generating profile: ' + error.message));
     };
 }
+/* jshint ignore:end */
 module.exports = profiler(path);
 
 
